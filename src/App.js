9285362f2fd04e64d39dbfb788b8b158ca9b1d57
Nick McMurdie <nickmcmurdie@gmail.com>
2021-05-10 23:56:46 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header'
+import Section from './components/Section'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Section>
+        Children
+      </Section>
+    </>
+  );
+}
+
+export default App;
