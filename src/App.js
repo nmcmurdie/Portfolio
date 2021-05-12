6cb8567cb14355a48fba9db09ecec95602b9d831
Nick McMurdie <nickmcmurdie@gmail.com>
2021-05-11 22:13:38 -0700

    Remove themes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import { useEffect } from 'react'
 import Header from './layouts/Header'
 import Section from './layouts/Section'
 import TitleSection from './layouts/sections/TitleSection'
 import AboutSection from './layouts/sections/AboutSection'
-import { Themes } from './Constants'
 import './App.css';
 
 
-function App() {
-  useEffect(() => {
-    let theme = Themes[Math.floor(Math.random() * Themes.length)];
-    document.documentElement.style.setProperty('--accent-primary', theme.primary);
-    document.documentElement.style.setProperty('--accent-secondary', theme.secondary);
-  }, []);
-  
+function App() {  
   return (
     <>
       <Header />
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Profile.css'
 
+const PROFILE_SIZE = 200;
+
 function Profile() {
     return (
-        <img className="profile" src={process.env.PUBLIC_URL + "/img/profile.jpg"} alt="Profile Icon" />
+        <img width={`${PROFILE_SIZE}px`} height={`${PROFILE_SIZE}px`} className="profile" src={process.env.PUBLIC_URL + "/img/profile.jpg"} alt="Profile Icon" />
     )
 }
 
