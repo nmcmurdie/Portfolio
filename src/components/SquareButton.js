dd672bf609965cf33716a6367e7a3dba35d1a1d3
Nick McMurdie <nickmcmurdie@gmail.com>
2021-05-11 17:01:09 -0700

    Add social media links

---
+++ src/components/SquareButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './SquareButton.css'
+
+function SquareButton({ icon, iconURL, alt, link }) {
+    return (
+        <form action={link}>
+            <button title={alt} className={`button button-square button-${alt.toLowerCase()}`}>
+                {icon && <span className="material-icons-outlined">{icon}</span>}
+                {iconURL && <img alt={alt} src={`${process.env.PUBLIC_URL}/img/${iconURL}`} />}
+            </button>
+        </form>
+    )
+}
+
+export default SquareButton
--- src/layouts/TitleSection.js
+++ src/layouts/TitleSection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Section from './Section'
 import Profile from '../components/Profile'
+import SquareButton from '../components/SquareButton'
 import './TitleSection.css'
 
 function TitleSection() {
@@ -8,7 +9,9 @@ function TitleSection() {
         <Section type="title" title="Nicholas McMurdie">
             <Profile />
             <div className="links-container">
-                
+                <SquareButton alt="Email" icon="email" link="mailto:nickmcmurdie@gmail.com" />
+                <SquareButton alt="Github" iconURL="github.svg" link="https://www.github.com/nmcmurdie" />
+                <SquareButton alt="LinkedIn" iconURL="linkedin.svg" link="https://www.linkedin.com/in/nick-mcmurdie" />
             </div>
         </Section>
     )
