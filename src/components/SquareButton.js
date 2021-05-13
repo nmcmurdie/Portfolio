cbd763060d77649a56171294d62aa94b11efea92
Nick McMurdie <nickmcmurdie@gmail.com>
2021-05-12 17:25:16 -0700

    Remove form elements from SquareButton

--- src/components/SquareButton.js
+++ src/components/SquareButton.js
@@ -3,12 +3,10 @@ import './SquareButton.css'
 
 function SquareButton({ icon, iconURL, alt, link, simple }) {
     return (
-        <form action={link}>
-            <button title={alt} className={`button button-square button-${alt.toLowerCase()}${simple ? ' button-simple' : ''}`}>
-                {icon && <span className="material-icons-outlined">{icon}</span>}
-                {iconURL && <img alt={alt} src={`${process.env.PUBLIC_URL}/img/${iconURL}`} />}
-            </button>
-        </form>
+        <a href={link} title={alt} className={`button button-square button-${alt.toLowerCase()}${simple ? ' button-simple' : ''}`}>
+            {icon && <span className="material-icons-outlined button-icon">{icon}</span>}
+            {iconURL && <img className="button-icon" alt={alt} src={`${process.env.PUBLIC_URL}/img/${iconURL}`} />}
+        </a>
     )
 }
 
